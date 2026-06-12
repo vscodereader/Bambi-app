36d3870ffa583707951f3c92d1b6c93a9341b6ae
dev <dev@mkvista.com>
2026-06-12 14:45:04 +0900

    프로젝트 초기화

---
+++ apps/web/src/lib/auth-client.ts
@@ -0,0 +1,6 @@
+import { env } from "@bambi-app/env/web";
+import { createAuthClient } from "better-auth/react";
+
+export const authClient = createAuthClient({
+  baseURL: env.NEXT_PUBLIC_SERVER_URL,
+});
---
+++ packages/auth/src/index.ts
@@ -0,0 +1,34 @@
+import { createDb } from "@bambi-app/db";
+import * as schema from "@bambi-app/db/schema/auth";
+import { env } from "@bambi-app/env/server";
+import { expo } from "@better-auth/expo";
+import { betterAuth } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+
+export function createAuth() {
+  const db = createDb();
+
+  return betterAuth({
+    database: drizzleAdapter(db, {
+      provider: "pg",
+
+      schema: schema,
+    }),
+    trustedOrigins: [env.CORS_ORIGIN, "bambi-app://", "exp://", "http://localhost:8081"],
+    emailAndPassword: {
+      enabled: true,
+    },
+    secret: env.BETTER_AUTH_SECRET,
+    baseURL: env.BETTER_AUTH_URL,
+    advanced: {
+      defaultCookieAttributes: {
+        sameSite: "none",
+        secure: true,
+        httpOnly: true,
+      },
+    },
+    plugins: [expo()],
+  });
+}
+
+export const auth = createAuth();
